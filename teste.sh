9bb7634d9c258a620f419733c50e0c69e7b4b702
voidcurser12 <104449547+voidcurser12@users.noreply.github.com>
2023-01-20 10:58:59 +0000

    Adicionado pedido do nome da pessoa e esse nome adicionado ao ficheiro txt. Adicionado funcionalidade para se houver mais do que uma pessoa no grupo, o script pergunta os nomes de todos até o user fazer input "para".

--- teste.sh
+++ teste.sh
@@ -8,6 +8,20 @@ echo $data
 echo "Insira nome do ficheiro: "
 read filename
 
+echo "Quantas pessoas fizeram o trabalho:"
+read nrp
+
+if [[ $nrp -gt 1 ]]
+then
+    echo "Insira quem fez o trabalho (quando não houver mais elementos do grupo insira 'para'):"
+    while true; do
+        read pessoa
+        if [[ $pessoa == "para" ]]; then
+            break
+        fi
+    done
+fi
+
 echo "Insira o nome do projeto: "
 read projeto
 echo "Insira o objetivo do projeto: "
@@ -16,4 +30,4 @@ echo "Qual é a disciplina: "
 read disciplina
 
 echo -e "Introdução: O nosso projeto $projeto tem como objetivo $objetivo,\nEste trabalho é para a disciplina de $disciplina." > $filename.txt
-echo -e "Data: $data " >> $filename.txt
+echo -e "Data: $data\nTrabalho realizado por: $pessoa" >> $filename.txt
