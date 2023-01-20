14060409aa2fa2adda711e2a2e83349d5d20d8f7
voidcurser12 <104449547+voidcurser12@users.noreply.github.com>
2023-01-20 10:04:31 +0000

    Adicionado template com variáveis, que seram input

--- teste.sh
+++ teste.sh
@@ -2,7 +2,16 @@
 
 #!/bin/bash
 
+#!/bin/bash
+
 echo "Insira nome do ficheiro: "
 read filename
 
-echo "A data currente é: $(date)" > $filename.txt
+echo "Insira o nome do projeto: "
+read projeto
+echo "Insira o obejtivo do projeto: "
+read objetivo
+echo "Qual é a disciplina: "
+read disciplina
+
+echo "Introdução: O nosso projeto $projeto tem como objetivo $objetivo, este trabalho é para a disciplina de $disciplina." > $filename.txt
