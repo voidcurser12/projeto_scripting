884ff42ece223ac853028192496016d537ea7ab5
voidcurser12 <104449547+voidcurser12@users.noreply.github.com>
2023-01-20 10:15:35 +0000

    Adicionado variavel da data atual e adicionado ao documento txt criado

--- teste.sh
+++ teste.sh
@@ -2,14 +2,18 @@
 
 #!/bin/bash
 
+data=$(date '+%d-%m-%Y')
+echo $data
+
 echo "Insira nome do ficheiro: "
 read filename
 
 echo "Insira o nome do projeto: "
 read projeto
-echo "Insira o obejtivo do projeto: "
+echo "Insira o objetivo do projeto: "
 read objetivo
 echo "Qual é a disciplina: "
 read disciplina
 
-echo "Introdução: O nosso projeto $projeto tem como objetivo $objetivo, este trabalho é para a disciplina de $disciplina." > $filename.txt
+echo -e "Introdução: O nosso projeto $projeto tem como objetivo $objetivo,\nEste trabalho é para a disciplina de $disciplina." > $filename.txt
+echo -e "Data: $data " >> $filename.txt
