4e87feb8c82ad5e105a1d418e247333d6b90983b
voidcurser12 <104449547+voidcurser12@users.noreply.github.com>
2023-01-23 10:48:02 +0000

    Adicionado mais uma variavel onde se vai guardar todos os elementos do grupo. Resolvido problema de não adicionar todos os elementos do grupo ao ficheiro txt

--- teste.sh
+++ teste.sh
@@ -11,6 +11,7 @@ read filename
 echo "Quantas pessoas fizeram o trabalho:"
 read nrp
 
+autores=()
 if [[ $nrp -gt 1 ]]
 then
     echo "Insira quem fez o trabalho (quando não houver mais elementos do grupo insira 'para'):"
@@ -19,15 +20,30 @@ then
         if [[ $pessoa == "para" ]]; then
             break
         fi
+        autores+=($pessoa)
     done
+else
+    echo "Insira quem fez o trabalho:"
+    read pessoa 
 fi
+
+echo ${autores[@]}
 
 echo "Insira o nome do projeto: "
 read projeto
+
 echo "Insira o objetivo do projeto: "
 read objetivo
+
 echo "Qual é a disciplina: "
 read disciplina
 
+#escrita no ficheiro txt
 echo -e "Introdução: O nosso projeto $projeto tem como objetivo $objetivo,\nEste trabalho é para a disciplina de $disciplina." > $filename.txt
-echo -e "Data: $data\nTrabalho realizado por: $pessoa" >> $filename.txt
+
+#Fazer outro if para quando é 1 ou mais que 1
+echo -n -e "Trabalho realizado por:\n${autores[0]}" > $filename.txt
+for ((i=1; i < ${#autores[@]}; i++))
+do
+  echo -n ", ${autores[i]}" >> $filename.txt
+done
